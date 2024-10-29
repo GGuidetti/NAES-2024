4c76a61cf05832dc076367b9aed009ff20c078b5
GGuidetti <gustavoo.guidetti@gmail.com>
2024-10-29 20:49:37 -0300

    Avaliação

--- paginas/views.py
+++ paginas/views.py
@@ -1,8 +1,27 @@
 
 from django.views.generic import TemplateView
+import random
+from cadastros.models import Receita
 
 class PaginaInicial(TemplateView):
     template_name = 'paginas/index2.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Página Inicial"
+        context['qtdeReceitas'] = Receita.objects.count()
+      
+        receitas = list(Receita.objects.order_by('nome'))
+        if receitas:
+            numero_receitas = min(3, len(receitas))
+            context['receitassAleatorias'] = random.sample(
+                receitas, numero_receitas)
+        else:
+            context['receitassAleatorias'] = []
+
+        print(context['receitassAleatorias'])
+
+        return context
  
 class SobreView(TemplateView):
     template_name = 'paginas/sobre.html'
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, reverse_lazy
 from django.contrib.auth import views
+from .views import UsuarioCreate
 
 urlpatterns = [
     path("entrar/", views.LoginView.as_view(
@@ -18,4 +19,9 @@ urlpatterns = [
             'titulo': 'Alterar senha'
         }
     ), name="alterar-senha"),
+    
+    path('registrar/', UsuarioCreate.as_view(
+        success_url=reverse_lazy("index"),
+        template_name="usuarios/form_registrar.html",
+    ), name='registrar')
 ]
